f4f0127669dcdf50c762ed62663e555a7bfb75e6
Allan Carlos <allancarlosdeveloper@gmail.com>
2019-04-07 09:59:06 -0300

    feat: Jumbotron with search field

--- index.php
+++ index.php
@@ -23,8 +23,42 @@
     </div>
   </nav>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <div class="jumbotron jumbotron-billboard">
+    <div class="img"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 px-xs-5 px-0 px-md-5">
+          <div class="input-group mb-3">
+            <img src="./assets/icons/search_btn.png" alt="">
+            <input type="text" class="form-control" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01" placeholder="Buscar pelo nome do produto">
+            <div class="input-group-append">
+              <button class="btn" type="button" id="button-addon2">
+                Buscar
+              </button>
+            </div>
+          </div>
+          
+          <div class="jumbotron__quote">
+            <div class="jumbotron__text">
+              <p>Busque produtos, compare os preços e escolha a <u>melhor opção</u></p>
+            </div>
+          
+            <div class="jumbotron__icon">
+              <img src="./assets/icons/arrow.svg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script>
+    setTimeout(function() {
+      // document.location.reload()
+    }, 8000);
+  </script>
 </body>
 </html>
