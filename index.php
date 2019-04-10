78418676d6315ddd5cea0477edd1dff5157001bf
Allan Carlos <allancarlosdeveloper@gmail.com>
2019-04-10 09:28:08 -0300

    refactor: deleting commented code

--- index.php
+++ index.php
@@ -1,32 +1,21 @@
 <?php
-
-// First, include Requests
 include('vendor/rmccue/requests/library/Requests.php');
+
 Requests::register_autoloader();
 
-// if ($_SERVER['REQUEST_URI'] == '/help') {
-//     include 'help.php';
-// } elseif ($_SERVER['REQUEST_URI'] == '/calendar') {
-//     include 'calendar.php';
-// } else {
-//     include 'notfound.php';
-// }
-
-
 if (isset($_SERVER['REDIRECT_URL'])) {
-$requested_page = $_SERVER['REDIRECT_URL'];
-}
-else {
-$requested_page = 'home';
+	$requested_page = $_SERVER['REDIRECT_URL'];
+} else {
+	$requested_page = 'home';
 }
 
 switch($requested_page) {
-   case "/search":
-      include(__DIR__."/search-results.php");
-      break;
-   case "home":
-      include(__DIR__."/home.php");
-      break;
-   default:
-      include(__DIR__."/404.php");
+	case "/search":
+		include(__DIR__."/search-results.php");
+		break;
+	case "home":
+		include(__DIR__."/home.php");
+		break;
+	default:
+		include(__DIR__."/404.php");
 }
